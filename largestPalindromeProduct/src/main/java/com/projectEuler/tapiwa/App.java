e430cc3c4be16af5dc0d5c71bb2f008eb02b298f
tapiwachadenga <tapiwachadenga@gmail.com>
2024-12-24 16:39:51 -0600

    pe4 largest palindrome product

--- largestPalindromeProduct/src/main/java/com/projectEuler/tapiwa/App.java
+++ largestPalindromeProduct/src/main/java/com/projectEuler/tapiwa/App.java
@@ -10,17 +10,9 @@ public class App
 {
     public static void main( String[] args )
     {
-	/*
-        System.out.print( "\nEnter start: " );
-	int start = new Scanner(System.in).nextInt();
-        System.out.print( "\nEnter end: " );
-	int end = new Scanner(System.in).nextInt();
-	System.out.printf("\nlargestPalindrome(%d, %d) = %d\n", start, end, Palindrome.largestPalindrome(start, end));
-	*/
 	System.out.printf("\nEnter number of digits: ");
 	int digits = new Scanner(System.in).nextInt();
 	
-	// for (int i = 1 ; i < 10; i++)
 	System.out.printf("\nlargestPalindromeProd(%d, %d) = %d\n", Palindrome.rangeFinder(digits)[0], Palindrome.rangeFinder(digits)[1], Palindrome.largestPalindromeProduct(digits));
     }
 }
