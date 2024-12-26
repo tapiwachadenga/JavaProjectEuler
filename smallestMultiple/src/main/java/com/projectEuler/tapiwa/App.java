58485e651783af4366597dab4997e6bc8f2c44e1
tapiwachadenga <tapiwachadenga@gmail.com>
2024-12-26 13:34:20 -0600

    pe5 smallest common multiple

--- smallestMultiple/src/main/java/com/projectEuler/tapiwa/App.java
+++ smallestMultiple/src/main/java/com/projectEuler/tapiwa/App.java
@@ -11,15 +11,15 @@ public class App
 {
     public static void main( String[] args )
     {
+        System.out.println();
         System.out.print( "Enetr lower limit: " );
 	String lLimit = new Scanner(System.in).next();
         System.out.print( "Enetr upper limit: " );
 	String uLimit = new Scanner(System.in).next();
 	BigInteger[] range = SmallestCommonMultiple.smallestCommonMultipleRange(lLimit, uLimit);
 
-	System.out.printf( "\nsmallestCommonMultiple(%s, %s) = %s\n", lLimit, uLimit, SmallestCommonMultiple.smallestCommonMultiple(range) );
-	
-	// for (int i = 0; i < range.length; i++)
-	//	System.out.printf("%s ", range[i].toString());
+        System.out.println();
+	System.out.printf( "\nsmallestCommonMultiple(%s, %s) = %s\n", lLimit, uLimit, SmallestCommonMultiple.smallestCommonMultiple(range) );	
+        System.out.println();
     }
 }
